1ab6af4a7f6335f4dc6337dc1583bddd8260434c
Feng Huatao <fhtzvl@163.com>
2022-04-08 16:05:51 +0800

    Add code comments

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-set -x
+set -x  # print following script logs to screen
 
-PYTHON=${PYTHON:-"python"}
+PYTHON=${PYTHON:-"python"}  # ":-"表示缺省值. ${var:-string}:若变量var为空或者未定义,则用在命令行中用string来替换${var:-string} 
 
 METHOD=$1
 WORK_DIR=$2
-PY_ARGS=${@:3}
+PY_ARGS=${@:3}  # 脚本的所有参数中, 从第3个位置[0,1,2,3,...]开始的子字符串
 
 GPUS=${GPUS:-4}
 
@@ -19,6 +19,8 @@ do
     fi
 done
 
+# https://pytorch.org/docs/stable/distributed.html#launch-utility
+# torch.distributed.launch: 为每个训练节点启动多个进程实现分布式训练
 $PYTHON -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT --use_env \
 $METHOD/main.py $METHOD/config.yaml --work-dir=${WORK_DIR} \
     --launcher="pytorch" --tcp-port=${PORT} --set ${PY_ARGS}
